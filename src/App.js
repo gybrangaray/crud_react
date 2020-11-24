51e343963639d69ab1d7893387b6211c3dcaa206
gybran <gybrangaray@gmail.com>
2020-11-24 11:17:55 -0600

    name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         este sera un crud 20211
+         este sera un crud garay gamboa 20211
         </p>
         
       </header>
