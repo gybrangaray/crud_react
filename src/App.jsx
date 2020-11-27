36cc49db832176703373eb9e741173590955058a
OsvaldoJacome <osvaldo.eternec@gmail.com>
2020-11-27 10:03:10 -0600

    a

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Jacome & Gybran Garay
+         Jácome & Gybran Garay
         </p>
         
       </header>
