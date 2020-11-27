c3e75d1f6604b9f83f9f3929105ab845e71f33bd
OsvaldoJacome <osvaldo.eternec@gmail.com>
2020-11-27 09:45:29 -0600

    commit a

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Template para crear CRUD by Osvaldo Jacome & Gybran Garay
-        </p>
+        
         
       </header>
     </div>
