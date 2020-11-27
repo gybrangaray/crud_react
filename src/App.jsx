6b8404d58d2ff0ccb0ea6f707fd82595728067af
gybran <gybrangaray@gmail.com>
2020-11-27 10:13:02 -0600

    camara

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Jácome & Gybran Garay
+         Camara gordo no mms
         </p>
         
       </header>
